96f091c62ae5998a7775543ae4da02b5f15980f9
Martin Schlecker <martin.schlecker@tum.de>
2018-02-15 17:20:56 +0100

    Create new script for plots

---
+++ planetePlots.py
@@ -0,0 +1,9 @@
+""" Plotting routines for planet population synthesis and analysis of results
+from the planet formation Code 'Planete' by the Bern planet formation group.
+
+Written by: Martin Schlecker
+schlecker@mpia.de
+"""
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
