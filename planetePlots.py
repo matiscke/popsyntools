2b3b0224fc4eed958ffa6cb51239771759256593
Martin Schlecker <schlecker@mpia.de>
2018-03-23 21:55:12 +0100

    normalize occurrence rate

--- planetePlots.py
+++ planetePlots.py
@@ -35,6 +35,26 @@ sns.set(context='notebook', style='whitegrid', font_scale=1., palette='colorblin
 })
 sns.set_color_codes()
 
+
+def normalize_rate(n_planet, n_star):
+    """ normalize the occurrence rate to planets per 100 stars.
+
+    Parameters
+    ----------
+    n_planet : int
+        number of planets
+    n_star : int
+        number of stars
+
+    Returns
+    -------
+    norm_rate : float
+        normalized occurrence rate
+    """
+    norm_rate = 100*n_planet/n_star
+    return norm_rate
+
+
 def plot_occurrence(population, ax=None, xAxis='a', yAxis='r',*funcArgs, **funcKwargs):
     """Plot an occurrence map in two parameters.
 
