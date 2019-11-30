b9090ea80e71d6b7dd9d1753ae41902877e92db5
Martin Schlecker <schlecker@mpia.de>
2019-11-30 10:24:53 +0100

    cherry-pick current plotstyle from master branch
    (cherry picked from commit 3044b252b6ff256279917c2a13338fcfad8aa01d)

--- popsyntools/plotstyle.py
+++ popsyntools/plotstyle.py
@@ -1,50 +1,48 @@
 """ Config file with my personal preference of default matplotlibrc settings.
 """
+import matplotlib as mpl
 from matplotlib import rcParams
 from cycler import cycler
-import seaborn as sns
-
-
-def styleplots():
-    # Set plot style
-    sns.set(context='notebook', style='ticks', font_scale=1.2, palette='Set2',\
-        rc={
-    # 'text.usetex':True,
-    # 'text.latex.unicode':True,
-    'font.family' : 'sans-serif',
-    #'font.serif':'Computer Modern',
-    'font.style'         : 'normal',
-    'font.variant'        : 'normal',
-    'font.weight'         : 'normal',
-    'font.stretch'        : 'normal',
-    'savefig.dpi'         : 400,
-    'lines.linewidth'     : 2.0,
-    'figure.figsize'   : [6.4, 4.8],         # figure size in inches
-    'figure.facecolor'      : 'white',
-    'figure.subplot.left'    : 0.17,    # the left side of the subplots of the figure
-    'figure.subplot.bottom'  : 0.18,   # the bottom of the subplots of the figure
-    'figure.subplot.right'   : 0.96,   # the right side of the subplots of the figure
-    'figure.subplot.top'     : 0.93,    # the top of the subplots of the figure
-    'figure.subplot.hspace'  : 0.0,    # height reserved for space between subplots
-    'axes.xmargin' : 0.02,             # default margin for autoscale
-    'axes.ymargin' : 0.02,
-    'legend.handletextpad' : 0.5,
-    'legend.handlelength' : 0.75,
-    'xtick.minor.size'     : 2.,
-    'ytick.minor.size'     : 2.,
-    'image.cmap'   : 'inferno',
-    # histograms
-    'hist.bins' : 20,
-    'patch.edgecolor' : 'black',
-    })
-
-    sns.set_color_codes()
 
 
 def get_colorPalette():
     """ return a custom made color palette."""
     return ['#008fd5', '#fc4f30', '#e5ae38',
         '#810f7c', '#029e73', '#8b8b8b', '#00035b', '#fe828c','#005249']
+
+
+def styleplots():
+    # Set plot style
+    spinewidth = 1.5
+
+    font = {'family' : 'normal',
+            'weight' : 'normal',
+            'size'   : 9}                 # 9 pt corresponds to caption size of 1-col A&A
+    legend = {'handlelength' : 0.75,
+              'handletextpad' : 0.5}
+    figure = {'dpi' : 100,
+              'subplot.left'    : 0.16,   # the left side of the subplots of the figure
+              'subplot.bottom'  : 0.21,   # the bottom of the subplots of the figure
+              'subplot.right'   : 0.98,   # the right side of the subplots of the figure
+              'subplot.top'     : 0.97,   # the top of the subplots of the figure
+              'subplot.hspace'  : 0.0}    # height reserved for space between subplots
+
+    mpl.rc('font', **font)
+    mpl.rc('legend', **legend)
+    mpl.rc('figure', **figure)
+    mpl.rc('lines', linewidth = 2.5)
+
+    mpl.rc('axes', linewidth = spinewidth)
+    mpl.rc('xtick.major', width = spinewidth)
+    mpl.rc('ytick.major', width = spinewidth)
+    mpl.rc('xtick.minor', width = .5*spinewidth)
+    mpl.rc('ytick.minor', width = .5*spinewidth)
+    mpl.rc('image', cmap = 'inferno')
+    mpl.rc('hist', bins = 20)
+    mpl.rc('patch', edgecolor = 'black')
+    mpl.rc('savefig', bbox = 'tight', dpi = 200)
+
+    rcParams['axes.prop_cycle'] = cycler(color=get_colorPalette())
 
 
 def histKwargs(overrideDict=None):
@@ -63,5 +61,54 @@ def histKwargs(overrideDict=None):
     return histKwargs
 
 
+def set_size(width='aa', subplot=[1,1], scale=1):
+    """ Set aesthetic figure dimensions to avoid scaling in latex.
+
+    Parameters
+    ----------
+    width: float or string
+        either width in pts or one of the following strings:
+        'aa' : A&A column width
+        'aaDouble' : A&A total text width
+    subplot : list
+        subplot dimensions in [rows, columns]
+    scale: float
+        fraction of the width which you wish the figure to occupy
+
+    Returns
+    -------
+    fig_dim: tuple
+        Dimensions of figure in inches
+
+    Credits
+    -------
+    Adapted from https://jwalton.info/Embed-Publication-Matplotlib-Latex/
+    """
+    aa_textwidth = 523.53       # textwidth of A&A template, as measured with "/the/textwidth" Latex command
+    if width == 'aaDouble':
+        width_pt = aa_textwidth
+    elif width == 'aa':
+        width_pt = aa_textwidth/2
+    else:
+        width_pt = width
+
+    # Width of figure
+    fig_width_pt = width_pt * scale
+
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+
+    # Golden ratio to set aesthetic figure height
+    golden_ratio = (5**.5 - 1) / 2
+
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+
+    # Figure height in inches
+    fig_height_in = fig_width_in * golden_ratio * (subplot[0] / subplot[1])
+    fig_dim = [fig_width_in, fig_height_in]
+
+    return fig_dim
+
+
 styleplots()
-rcParams['axes.prop_cycle'] = cycler(color=get_colorPalette())
